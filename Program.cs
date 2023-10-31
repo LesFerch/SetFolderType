62d2b86861bf7236501b2629a8a86d3cdb5145f2
LesFerch <79026235+LesFerch@users.noreply.github.com>
2023-10-30 22:59:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SetFolderType
+{
+    public class ExtractData
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFile);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FreeLibrary(IntPtr hModule);
+
+        public string ExtractStringFromDLL(string file, int number)
+        {
+            IntPtr lib = LoadLibrary(file);
+            StringBuilder result = new StringBuilder(2048);
+            LoadString(lib, number, result, result.Capacity);
+            FreeLibrary(lib);
+            return result.ToString();
+        }
+    }
+    public class IniFileHelper
+    {
+        private string filePath;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
+
+        public IniFileHelper(string path)
+        {
+            filePath = path;
+        }
+
+        public void WriteValue(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, filePath);
+        }
+
+        public string ReadValue(string section, string key)
+        {
+            const int size = 255;
+            System.Text.StringBuilder builder = new System.Text.StringBuilder(size);
+            GetPrivateProfileString(section, key, "", builder, size, filePath);
+            return builder.ToString();
+        }
+
+        public void UpdateValue(string section, string key, string value)
+        {
+            WriteValue(section, key, value);
+        }
+    }
+
+    class Program
+    {
+        static string myName = typeof(Program).Namespace;
+        static string lang = "en-US";
+        static string[] types = { "Generic", "Documents", "Pictures", "Music", "Videos", "None", "Help" };
+        static string[] labels = { "General items", "Documents", "Pictures", "Music", "Videos", "(None)", "Help" };
+        static string[] cmds = { "/g", "/d", "/p", "/m", "/v", "/x", "/h" };
+        static string sMain = "Set folder type to";
+        static string sYes = "Yes";
+        static string sNo = "No";
+        static string sInstall = "Install";
+        static string sRemove = "Remove";
+        static string sComplete = "Done";
+        static string sLocalDisk = "Local Disk";
+        static string sNTFS = "NTFS";
+
+        static void Main(string[] args)
+        {
+            lang = GetLang();
+            if (lang.Substring(0, 2) != "en") { GetStrings(); }
+
+            if (args.Length == 0)
+            {
+                InstallRemove();
+                return;
+            }
+
+            string Folder = "";
+            string command = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("/")) { command = args[i].ToLower(); }
+                else {
+                    Folder = args[i].Replace("\"","").Trim();
+                }
+            }
+
+            if (command == "/install") { InstallContextMenuEntries(); }
+
+            if (command == "/remove") { RemoveContextMenuEntries(); }
+
+            if (Folder == "") { return; }
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (command == cmds[i])
+                {
+                    if (command == "/h")
+                    {
+                        Process.Start("https://lesferch.github.io/SetFolderType/");
+                    }
+                    else
+                    {
+                        string driveLetter = Path.GetPathRoot(Folder).TrimEnd('\\');
+
+                        if ((command == "/x") || DriveOK(driveLetter))
+                        {
+                            ApplyFolderTypeRecursive(types[i], Folder);
+                        }
+                        else
+                        {
+                            MessageBox.Show($"{driveLetter}\n\n{sLocalDisk}\n{sNTFS}", myName);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void InstallRemove()
+        {
+            DialogResult result = MessageBox.Show($"{sYes} = {sInstall}\n\n{sNo} = {sRemove}", myName, MessageBoxButtons.YesNoCancel);
+
+            if (result == DialogResult.Yes)
+            {
+                InstallContextMenuEntries();
+                MessageBox.Show(sComplete, myName);
+            }
+            if (result == DialogResult.No)
+            {
+                RemoveContextMenuEntries();
+                MessageBox.Show(sComplete, myName);
+            }
+        }
+        static bool DriveOK(string driveLetter)
+        {
+            bool bOK = true;
+
+            // Check if the drive type is "Local Disk"
+            DriveInfo driveInfo = new DriveInfo(driveLetter);
+
+            if (driveInfo.DriveType == DriveType.Fixed)
+            {
+                sLocalDisk = $"✓ {sLocalDisk}";
+            }
+            else
+            {
+                sLocalDisk = $"✗ {sLocalDisk}";
+                bOK = false;
+            }
+
+            // Check if drive is formatted NTFS
+            if (IsNTFS(driveLetter))
+            {
+                sNTFS = $"✓ {sNTFS}";
+            }
+            else
+            {
+                sNTFS = $"✗ {sNTFS}";
+                bOK = false;
+            }
+
+            return bOK;
+        }
+
+        static string GetLang()
+        {
+            string lang = "en-US";
+
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\International");
+                if (key != null)
+                {
+                    lang = key.GetValue("LocaleName") as string;
+                    key.Close();
+                }
+            }
+            catch { }
+
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop");
+                if (key != null)
+                {
+                    string[] preferredLanguages = key.GetValue("PreferredUILanguages") as string[];
+                    if (preferredLanguages != null && preferredLanguages.Length > 0)
+                    {
+                        lang = preferredLanguages[0];
+                    }
+                    key.Close();
+                }
+            }
+            catch { }
+
+            return lang;
+        }
+        static void GetStrings()
+        {
+            string file = $@"C:\Windows\System32\{lang}\Shell32.dll.mui";
+
+            if (!File.Exists(file))
+            {
+                MessageBox.Show($"Language file not found:\n\n{file}", myName);
+                return;
+            }
+
+            sYes       = GetString(file, 33224); // Yes
+            sNo        = GetString(file, 33232); // No
+            sInstall   = GetString(file, 10210); // Install
+            sRemove    = GetString(file, 38314); // Remove
+            sComplete  = GetString(file, 12574); // Finished
+            sLocalDisk = GetString(file,  9315); // Local Disk
+            sMain      = GetString(file,  6495); // Folder View
+            labels[0]  = GetString(file, 29990); // General items
+            labels[1]  = GetString(file, 21770); // Documents
+            labels[2]  = GetString(file, 17451); // Pictures
+            labels[3]  = GetString(file, 17450); // Music
+            labels[4]  = GetString(file, 17452); // Videos
+            labels[5]  = GetString(file,  4256); // (None)
+            labels[6]  = GetString(file, 30489); // Help
+        }
+        static string GetString(string file, int num)
+        {
+            ExtractData ed = new ExtractData();
+            string sLocStr = ed.ExtractStringFromDLL(file, num);
+            sLocStr = sLocStr.Replace("&", "");
+            // Remove and keyboard shortcut indicator from string
+            if (sLocStr.Length > 3) { sLocStr = Regex.Replace(sLocStr, @"\((.)\)", ""); }
+            return sLocStr;
+        }
+        static void InstallContextMenuEntries()
+        {
+
+            RemoveContextMenuEntries();
+
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Classes\Directory\shell\SetFolderType"))
+            {
+                key.SetValue("SubCommands", "");
+                key.SetValue("", "");
+                key.SetValue("MUIVerb", sMain);
+                key.SetValue("Icon", exePath);
+            }
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                using (RegistryKey subKey = Registry.CurrentUser.CreateSubKey($@"Software\Classes\Directory\shell\SetFolderType\shell\{i}-{types[i]}"))
+                {
+                    subKey.SetValue("", labels[i]);
+
+                    using (RegistryKey commandKey = subKey.CreateSubKey("command"))
+                    {
+                        commandKey.SetValue("", $"\"{exePath}\" {cmds[i]} %v");
+                    }
+                }
+            }
+        }
+
+        static void RemoveContextMenuEntries()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\Directory\shell", true))
+            {
+                try { key.DeleteSubKeyTree("SetFolderType", false); }
+                catch { }
+            }
+        }
+
+        static void ApplyFolderTypeRecursive(string folderType, string directory)
+        {
+            // Apply to the selected folder
+            try { ApplyFolderType(directory, folderType); }
+            catch { }
+
+            // Recursively apply to subdirectories
+            string[] subDirectories = Directory.GetDirectories(directory);
+            foreach (string subDirectory in subDirectories)
+            {
+                try { ApplyFolderTypeRecursive(folderType, subDirectory); }
+                catch { }
+            }
+        }
+
+        static void ApplyFolderType(string directory, string folderType)
+        {
+            string desktopIniPath = Path.Combine(directory, "desktop.ini");
+
+            if (folderType == "None")
+            {
+                if (File.Exists(desktopIniPath)) { File.Delete(desktopIniPath); }
+                return;
+            }
+
+            if (File.Exists(desktopIniPath))
+            {
+                UpdateValue(desktopIniPath, "ViewState", "FolderType", folderType);
+            }
+            else
+            {
+                CreateIniFile(desktopIniPath);
+                UpdateValue(desktopIniPath, "ViewState", "FolderType", folderType);
+                File.SetAttributes(desktopIniPath, File.GetAttributes(desktopIniPath) | FileAttributes.System | FileAttributes.Hidden);
+            }
+        }
+
+        static void CreateIniFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("[ViewState]");
+                writer.WriteLine("FolderType=Generic");
+            }
+
+        }
+
+        static void UpdateValue(string filePath, string section, string key, string value)
+        {
+            IniFileHelper iniFile = new IniFileHelper(filePath);
+            iniFile.UpdateValue(section, key, value);
+        }
+
+        static bool IsNTFS(string driveLetter)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Volume");
+
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                string queryDriveLetter = (string)queryObj["DriveLetter"];
+                if (queryDriveLetter != null && queryDriveLetter.Equals(driveLetter, StringComparison.OrdinalIgnoreCase))
+                {
+                    string fileSystem = (string)queryObj["FileSystem"];
+                    if (fileSystem != null && fileSystem.Equals("NTFS", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # SetFolderType
-Set Windows Explorer folder types using Desktop.ini files
+
+[![image](https://user-images.githubusercontent.com/79026235/152910441-59ba653c-5607-4f59-90c0-bc2851bf2688.png)Download the zip file](https://github.com/LesFerch/SetFolderType/releases/download/1.0.0/SetFolderType.zip)
+
+## Set Windows Explorer folder types using Desktop.ini files
+
+This program adds a right-click context menu to Windows Explorer that allows you to set the folder type for an entire folder tree by creating (or editing existing) **desktop.ini** files in each folder.
+
+The added context menu is created with registry entries only and simply provides submenus entries for each folder type. When one of those items are selected, SetFolderType.exe is run with the appropriate arguments to set the selected folder type for the selected folder tree.
+
+This program does NOT create a context menu handler. That is, there is no code that runs when you right-click a folder. Code only runs when you actually select an action (i.e. select a folder type that you want applied). SetFolderType will add no overhead to your context menu, other than the insignificant impact of one more context menu item.
+
+## Why set folder types using Desktop.ini files?
+
+### Background Information
+
+Windows supports setting the folder type to one of **General items**, **Documents**, **Pictures**, **Music**, or **Videos** with the appropriate entry in a **hidden**, **system** file named **desktop.ini**. This has the advantage of making the folder type setting permanent to the folder itself. You can reset your profile, create a new user, or reset Windows, and the folder types will be remembered and displayed with whatever view you have chosen for that folder type.
+
+**Note**: To globallly set your preferred folder type views, please see [WinSetView](https://lesferch.github.io/WinSetView/)
+
+However, there is a catch. Explorer only provides this functionality on drives that mount as **Local Disk** (aka "fixed disk") and are formatted **NTFS**. Therefore, this tool will work on all internal drives that are formatted **NTFS**, but it will only work on some USB drives. For example, SanDisk USB SSD drives mount as type **Local Disk**, so they will work with this tool if the drive is formatted to **NTFS**.
+
+### Partial Fix for Windows 11
+
+For **Windows 11** this tool provides a partial fix for some broken funtionality in the Windows 11 Explorer. Specifically, the feature **Also apply this template to all subfolders** is completely broken in Windows 11 (assuming you have not patched Windows 11 to run the Windows 10 Explorer). Here's a [video](https://www.youtube.com/watch?v=U5eEFNZEWZg) demonstrating the issue.
+
+The fix is partial because, as noted above, it only applies to drives of type **Local Disk** that are formatted **NTFS**. For such drives, SetFolderType will give you back the ability to set the folder type for an entire folder tree.
+
+### Permanent folder type setting for both Windows 10 and Windows 11
+
+For **Windows 10** (or older) or [Windows 10 Explorer on Windows 11](https://lesferch.github.io/OldExplorer/), the **Also apply this template to all subfolders** feature works fine, but there is still a scenario wher you may want this tool.
+
+As noted in the **Background** section above, setting the folder type via **desktop.ini** files makes the folder type setting permanent because it's part of the folder, instead of just a registry setting.
+
+# How to Download and Install
+
+1. Download the zip file using the link above.
+2. Extract **SetFolderType.exe**.
+3. Right-click **SetFolderType.exe**, select Properties, check **Unblock**, and click **OK**.
+4. Move **SetFolderType.exe** to the folder of your choice (you need to keep this file).
+5. Double-click **SetFolderType.exe** to open the Install/Remove dialog and click **Yes** to install.
+6. If you skipped step 3, then, in the SmartScreen window, click More info and then Run anyway.
+7. Click **OK** when the **Done** message boc appears.
+
+**Note**: Some antivirus software may falsely detect the download as a virus. This can happen anytime you download a new executable and may require extra steps to whitelist the file.
+
+# How to Use
+
+Right-click any folder and you should see the SetFolderType context menu:
+
+![image](https://github.com/LesFerch/SetFolderType/assets/79026235/353cb8a6-5d36-41cb-aecc-8051815c3a01)
+
+\
+\
+[![image](https://user-images.githubusercontent.com/79026235/153264696-8ec747dd-37ec-4fc1-89a1-3d6ea3259a95.png)](https://github.com/LesFerch/SetFolderType)
